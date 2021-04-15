dde2a94b22124d93d8e8d8f19e9a538e2b12bead
ethomps33 <e.thomps33@gmail.com>
2021-04-15 09:58:48 -0700

    Outlined what is needed to accomplished

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,9 +1,13 @@
-// Bar Chart
+//Bar Chart
 
 //Bubble Chart
 
 //Display Individual Demographic Data
 
-//Display Key-Value Pairs from Metadata
+//Display Key-Value Pairs from Metadata id = sample-metadata
 
-//Update when Dropdown Changes
+//Update when Dropdown Changes: optionChanged function
+
+//Add options for drowpdown id = selDataset
+
+//init function
