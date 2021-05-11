cb90f3a023acce6733f62feab171abce1eedf0f4
ethomps33 <e.thomps33@gmail.com>
2021-05-11 14:18:36 -0700

    End code. Dropdown not working and charts not showing

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,45 +1,55 @@
+// select the user input field
+var idSelect = d3.select("#selDataset");
 
+// select the demographic info div's ul list group
+var demographicsTable = d3.select("#sample-metadata");
 
-//Bubble Chart
+// select the bar chart div
+var barChart = d3.select("#bar");
 
-//Display Individual Demographic Data
+// select the bubble chart div
+var bubbleChart = d3.select("bubble");
 
-//Display Key-Value Pairs from Metadata id = sample-metadata
 
 //Update when Dropdown Changes: optionChanged function
+function optionChanged(sampleNumber) {
+    d3.json("samples.json").then((data) => {
+        var name = data.names;
+        var sample = name.filter(d => d.id == sampleNumber);
+        var dropdown_object = d3.select("#selDataset");
+        sample.forEach(function(id) {
+            var id_option=dropdown_object.append("sample");
+            id_option.attr("value",id);
+            id_option.text(id);
+        })
 
+    });
+};
+optionChanged();
 //Add options for drowpdown id = selDataset
 
 //init function
-
-function buildMetaData(sampleNumber) {
-    d3.json("samples.json").then((data) => {
-        var metadata = data.metadata;
-        console.log(metadata);
-        //Filtering metadata
-        var sample =metadata.filter(metadata => metadata.id == sampleNumber);
-        //Getting rid of the array so it only displays dictionary
-        sample = sample[0];
-        //Selecting part of html where the data will display
-        var metadata_object = d3.select("#sample-metadata");
-        metadata_object.html("")
-        //Create a loop to append data
-        Object.entries(sample).forEach(([key, value]) => {
-            metadata_object.append("h6").text(`${key}: ${value}`);
-        })
-
-    })
-}
+function init() {
+    resetData();
 //Charts
-function buildCharts(sampleNumber) {
-    d3.json("samples.json").then((data) => {
-        sample = data.samples;
-        var sample = sample.filter(d => d.id == sampleNumber);
-        sample = sample[0];
-        var otu_ids = sample.otu_ids;
-        var otu_labels = sample.otu_labels;
-        var sample_values = sample.sample_values;
-    //Creating Bar Chart
+d3.json("data/samples.json").then((data => {
+
+   
+    //  use a forEach to loop over each name in the array data.names to populate dropdowns with IDs
+    data.names.forEach((name => {
+        var option = idSelect.append("option");
+        option.text(name);
+    })); 
+
+    // get the first ID from the list for initial charts as a default
+    var initId = idSelect.property("value")
+
+    // plot charts with initial ID
+    plotCharts(initId);
+
+})); 
+}
+    //Creating Bar Chart to display first 10 OTUs found in an individual
         barTrace = {
             x: sample_values.slice(0,10),
             y: otu_ids.slice(0,10).map(otu => `OTU ${otu}`).reverse(),
@@ -71,12 +81,80 @@ function buildCharts(sampleNumber) {
     };
 
     Plotly.newPlot("bubble", [bubbleTrace, layout]);
+    
+
+//create a function to reset divs to prepare for new data
+function resetData() {
+
+    demographicsTable.html("");
+    bar.html("");
+    bubble.html("");
+    gauge.html("");
+
+};
+
+//Display Individual Demographic Data & Display Key-Value Pairs from Metadata id = sample-metadata
+function buildMetaData(sampleNumber) {
+    d3.json("samples.json").then((data) => {
+        var metadata = data.metadata;
+        //Filtering metadata
+        var sample =metadata.filter(metadata => metadata.id == sampleNumber);
+        //Getting rid of the array so it only displays dictionary
+        sample = sample[0];
+        //Selecting part of html where the data will display
+        var metadata_object = d3.select("#sample-metadata");
+        metadata_object.html("")
+        //Create a loop to append data
+        Object.entries(sample).forEach(([key, value]) => {
+            metadata_object.append("h6").text(`${key}: ${value}`);
+        });
+
     });
 };
-buildCharts(940);
-buildMetaData(940);
-// * Use `sample_values` as the values for the bar chart.
 
-// * Use `otu_ids` as the labels for the bar chart.
+// filter the samples for the ID chosen
+var individualSample = data.samples.filter(sample => sample.id == id)[0];
 
-// * Use `otu_labels` as the hovertext for the chart.
+// create empty arrays to store sample data
+var otuIds = [];
+var otuLabels = [];
+var sampleValues = [];
+
+// Iterate through each key and value in the sample to retrieve data for plotting
+Object.entries(individualSample).forEach(([key, value]) => {
+
+    switch (key) {
+        case "otu_ids":
+            otuIds.push(value);
+            break;
+        case "sample_values":
+            sampleValues.push(value);
+            break;
+        case "otu_labels":
+            otuLabels.push(value);
+            break;
+            // case
+        default:
+            break;
+    }; 
+
+}); 
+
+
+
+// when there is a change in the dropdown select menu, this function is called with the ID as a parameter
+function optionChanged(id) {
+
+    // reset the data
+    resetData();
+
+    // plot the charts for this id
+    plotCharts(id);
+
+    // build metaData table
+    buildMetaData(id);
+};
+
+optionChanged();
+init();
+
